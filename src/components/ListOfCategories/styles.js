b78d5dc79534a163866f26cc60a0f048c362dab3
AlexK <alexjhon@live.com>
2019-10-19 11:51:39 -0500

    UI with styled-components

---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  display: flex;
+  overflow: scroll;
+  width: 100%;
+  overflow: -moz-scrollbars-none;
+  -ms-overflow-style: none;
+  width: 100%;
+  &::-webkit-scrollbar {
+    width: 0 !important;
+  }
+`;
+
+export const Item = styled.li`
+  padding: 0 8px;
+`;
